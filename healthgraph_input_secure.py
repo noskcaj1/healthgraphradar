e7bc62f94ee60905017adca1cba1ba9772229326
Jackson <jackson.sbm@gmail.com>
2025-07-13 20:15:24 -0300

    security_update_user

--- healthgraph_input_secure.py
+++ healthgraph_input_secure.py
@@ -5,7 +5,7 @@ def login():
     usuario = st.sidebar.text_input("Usuário")
     senha = st.sidebar.text_input("Senha", type="password")
     if st.sidebar.button("Entrar"):
-        if usuario == "admin" and senha == "admin123":
+        if usuario == "admin" and senha == "hgr@123":
             return True
         else:
             st.sidebar.error("Usuário ou senha inválidos.")
